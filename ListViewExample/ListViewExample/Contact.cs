d49d52f3c5564aea1f486631ec19d5018a071ee2
Miguel Angel <miguel@gmail.com>
2022-10-08 23:57:06 -0500

    Agregar archivos de proyecto.

---
+++ ListViewExample/ListViewExample/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListViewExample
+{
+    public class Contact: List<Employee>
+    {
+        public string Title { get; set; }
+        public string ShortName { get; set; }
+        public Contact(string title, string shortName)
+        {
+            Title = title;
+            ShortName = shortName;
+        }
+        public static IList<Contact> All { get; set; }
+    }
+}
---
+++ ListViewExample/ListViewExample/ListViewGroup.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace ListViewExample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewGroup : ContentPage
+    {
+        ObservableCollection<Contact> people = new ObservableCollection<Contact>();
+
+        public ObservableCollection<Contact> People { get { return people; } }
+        public ListViewGroup()
+        {
+            InitializeComponent();
+            GroupView.ItemsSource = people;
+            people.Add(new Contact("Angel", "A")
+            {
+                new Employee{ DisplayName = "Antonio"}
+            });
+            people.Add(new Contact("Banderas", "B")
+            {
+                new Employee { DisplayName = "Bill Gath" },
+                new Employee { DisplayName = "Bruno Mars" }
+            });
+            people.Add(new Contact("Charlie", "C")
+            {
+                new Employee { DisplayName = "Cecilia" },
+                new Employee { DisplayName = "Christopher"}
+            });
+            people.Add(new Contact("Romeo", "R")
+            {
+                new Employee { DisplayName = "Roberto Gomez" }
+            });
+            people.Add(new Contact("Sesar", "S")
+            {
+                new Employee { DisplayName = "Sharlie" }
+            });
+        }
+
+        
+    }
+}
